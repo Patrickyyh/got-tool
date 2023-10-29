38734a7f58d2af5748eca13821f3ec99f859d500
Patrickyyh <yeyuhao1234@tamu.edu>
2023-10-29 03:50:32 -0500

    update the implementation of the agents and agent executor.

---
+++ db_agents/main.py
@@ -0,0 +1,41 @@
+
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    MessagesPlaceholder ,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate
+)
+
+from langchain.agents import OpenAIFunctionsAgent , AgentExecutor
+from dotenv import load_dotenv
+load_dotenv()
+
+
+from tools.sql import run_query_tool
+
+chat = ChatOpenAI()
+prompt = ChatPromptTemplate(
+    input_variables=["input"],
+    messages=[
+        HumanMessagePromptTemplate.from_template("{input}"),
+        ## Consider the scratchpad as a very simple version of memory.
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+    ]
+)
+
+agent = OpenAIFunctionsAgent(
+    llm = chat,
+    prompt = prompt,
+    tools= [run_query_tool]
+)
+
+agent_executor = AgentExecutor(
+    agent=agent,
+    verbose=True,
+    tools = [run_query_tool]
+)
+
+
+agent_executor("How many users are in the database")
+
+
