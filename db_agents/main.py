5a3fa111aa16a63eca33b5e42b19057e4fbb30d9
Patrickyyh <yeyuhao1234@tamu.edu>
2023-10-29 06:26:02 -0500

    update

--- db_agents/main.py
+++ db_agents/main.py
@@ -7,35 +7,47 @@ from langchain.prompts import (
 )
 
 from langchain.agents import OpenAIFunctionsAgent , AgentExecutor
+from langchain.schema import SystemMessage
+
+
 from dotenv import load_dotenv
 load_dotenv()
 
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool , list_tables, describe_tables_tool
+
 
 chat = ChatOpenAI()
+tables = list_tables()
+print(tables)
+
 prompt = ChatPromptTemplate(
-    input_variables=["input"],
     messages=[
+        SystemMessage(content = (f"You are an AI that has access to a SQLite Database. \n"
+                                 f"The database has tables of: {tables} \n"
+                                 "Do not make any assumption about what tables exist "
+                                 "or what columns exist in each table. Instead, use the 'describe_tables' function"
+                    )),
         HumanMessagePromptTemplate.from_template("{input}"),
         ## Consider the scratchpad as a very simple version of memory
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
 
+tools = [run_query_tool , describe_tables_tool]
 agent = OpenAIFunctionsAgent(
     llm = chat,
     prompt = prompt,
-    tools= [run_query_tool]
+    tools= tools
 )
 
 agent_executor = AgentExecutor(
-    agent=agent,
+    agent= agent,
     verbose=True,
-    tools = [run_query_tool]
+    tools = tools
 )
 
 
-agent_executor("How many users are in the database")
+agent_executor("How many users have provided a shipping address?")
 
 
