a3dcf18ad8df4381d1a50c0289847302298d65b0
Patrickyyh <yeyuhao1234@tamu.edu>
2023-12-19 04:38:48 -0600

    implement the celery backend worker and update the document metadata

--- pdf-dist/app/chat/create_embeddings.py
+++ pdf-dist/app/chat/create_embeddings.py
@@ -2,21 +2,21 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from app.chat.vector_stores.pinecone import vectorstore
 
+"""
+TODO:
+Generate and store embeddings for the given pdf
+1. Extract text from the specified PDF.
+2. Divide the extracted text into manageable chunks.
+3. Generate an embedding for each chunk.
+4. Persist the generated embeddings.
+:param pdf_id: The unique identifier for the PDF.
+:param pdf_path: The file path to the PDF.
+Example Usage:
+create_embeddings_for_pdf('123456', '/path/to/pdf')
+"""
+
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
-    """
-    Generate and store embeddings for the given pdf
-    1. Extract text from the specified PDF.
-    2. Divide the extracted text into manageable chunks.
-    3. Generate an embedding for each chunk.
-    4. Persist the generated embeddings.
 
-    :param pdf_id: The unique identifier for the PDF.
-    :param pdf_path: The file path to the PDF.
-
-    Example Usage:
-
-    create_embeddings_for_pdf('123456', '/path/to/pdf')
-    """
     ## 1. Create a TextSpliter
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
@@ -29,8 +29,17 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     docs  = loader.load_and_split(text_splitter)
     ##print(docs)
 
+    #3. update the metadata to uniquely identify the vector by pdf's id. 
+    for doc in docs:
+        doc.metadata = {
+            "page": doc.metadata["page"],
+            "text": doc.page_content,
+            "pdf_id":pdf_id
+        }
+
     ## 3. Add the docs into the vector stores
     vectorstore.add_documents(docs)
+
 
 
 
