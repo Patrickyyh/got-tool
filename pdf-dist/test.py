e4bafd6923d4b0bca87466dbaef4fc81115c97cf
Patrickyyh <yeyuhao1234@tamu.edu>
2023-12-28 04:57:39 -0600

    implementation of the streaming Language model

--- pdf-dist/test.py
+++ pdf-dist/test.py
@@ -1,4 +1,3 @@
-
 from typing import Any, Dict, Iterator, Optional, Union
 from uuid import UUID
 from langchain.chat_models import ChatOpenAI
@@ -8,33 +7,40 @@ from langchain.callbacks.base import BaseCallbackHandler
 from dotenv import load_dotenv
 from langchain.schema.output import ChatGenerationChunk, GenerationChunk
 from langchain.schema.runnable.config import RunnableConfig
-from queue import  Queue
+from queue import Queue
 from threading import Thread
+
 load_dotenv()
 
 
 #
-queue = Queue()
+# queue = Queue()
 class StreamingHandler(BaseCallbackHandler):
+    def __init__(self, queue):
+        self.queue = queue
+
     def on_llm_new_token(self, token: str, **kwargs):
-        queue.put(token)
+        self.queue.put(token)
 
     def on_llm_end(self, response, **kwargs):
-        queue.put(None)
+        self.queue.put(None)
 
-    def on_llm_error(self,error, **kwargs: Any) -> Any:
-        queue.put(None)
+    def on_llm_error(self, error, **kwargs: Any) -> Any:
+        self.queue.put(None)
 
 
-chat = ChatOpenAI(model="gpt-4-1106-preview" ,streaming = True , callbacks=[StreamingHandler()])
-prompt = ChatPromptTemplate.from_messages([
-    ("human" ,"{content}")
-])
+chat = ChatOpenAI(model="gpt-4-1106-preview", streaming=True)
+prompt = ChatPromptTemplate.from_messages([("human", "{content}")])
 
-class StreamingChain(LLMChain):
-    def stream(self ,input):
+
+class StreamableChain(LLMChain):
+    def stream(self, input):
+        queue = Queue()
+        handler = StreamingHandler(queue)
+
         def task():
-            self(input)
+            self(input, callbacks=[handler])
+
         Thread(target=task).start()
         while True:
             if not queue.empty():
@@ -44,8 +50,11 @@ class StreamingChain(LLMChain):
                 yield token
 
 
+class StreamingChain(StreamableChain, LLMChain):
+    pass
+
 
 ## Queue object is thread safe in python.
-chain = StreamingChain(llm = chat , prompt=prompt )
-for output in  chain.stream(input = {"content" : "When to use asio in boost of c++"}):
+chain = StreamingChain(llm=chat, prompt=prompt)
+for output in chain.stream(input={"content": "please tell me a joke"}):
     print(output)
